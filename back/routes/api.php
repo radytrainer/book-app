a431f50a0be2ba0930228f3c754f0f9349302a99
radytrainer <rady.y@passerellesnumeriques.org>
2021-11-22 14:08:29 +0700

    update basic api

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,17 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\BookController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('books', [BookController::class, 'getAllBooks']);
+Route::get('books/{id}', [BookController::class, 'getBook']);
+
+Route::post('books', [BookController::class, 'createBook']);
+Route::put('books/{id}', [BookController::class, 'updateBook']);
+Route::delete('books/{id}', [BookController::class, 'deleteBook']);

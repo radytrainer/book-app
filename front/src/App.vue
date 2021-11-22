cfe1f8517bbcb9880de354401cdac45edda0c6c4
radytrainer <rady.y@passerellesnumeriques.org>
2021-11-22 12:34:44 +0700

    add card components

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <card></card>
+    <card></card>
+    <card></card>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Card from './components/ui/Card.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('card', Card);
+app.mount('#app')
